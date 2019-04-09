c9720a597305a71417a40ffa290e423e07f0ced0
Flávia Burgarelli Guimarães <flarelli@gmail.com>
2019-04-09 18:03:07 -0300

    Update Funções.cs

--- Funções.cs
+++ Funções.cs
@@ -10,21 +10,8 @@ namespace AutomacaoBDD.Functions
 {
     public class Funções
     {
-        public static List<string> RetornaFaseProposta(String proposta)
-        {
-            string retornaFaseProposta = "SELECT TOP 1 MPNRDECI FROM  BSOAUTORIZ..CMOVP WHERE MPNRPROP = @proposta".Replace("@proposta", proposta);
-            List<string> resultado = new List<string>(DatabaseFactory.DBRetornarDadosQuery(retornaFaseProposta));
-
-            if (resultado != null)
-            {
-                return resultado;
-            }
-            else
-            {
-                return null;
-            }            
-        }
-        protected static string RetornaTaxa(string value)
+        
+        protected static string RetornaDouble(string value)
         {
             string regex = "[0-9]{0,15}[,]{0,1}[0-9]{0,4}$";
             MatchCollection matches = new Regex(regex).Matches(value);
@@ -36,32 +23,20 @@ namespace AutomacaoBDD.Functions
 
             return null;
         }
-        public static double RecuperarMaiorTaxaProduto(string convenio)
+        
+        public static void CriarPrintEvidencias(string x, string pasta)
         {
-            TaxaConvenio.Negocio.ProdutoSimulacaoNegocio negocio = new TaxaConvenio.Negocio.ProdutoSimulacaoNegocio();
-
-            var produtosConvenio = negocio.RecuperarProdutosPort(convenio);
-
-            Tuple<string, double, int, int> produtoPortTaxaMaior = negocio.EncontrarMaiorTaxa(produtosConvenio);
-
-            double taxaConvenio = produtoPortTaxaMaior.Item2;
-            int prazoConvenio = produtoPortTaxaMaior.Item3;
-
-            return taxaConvenio;
-        }
-        public static void CriarPrintEvidencias(string numProposta, string pasta)
-        {
-            if (numProposta == null || numProposta == "")
+            if (x == null || x == "")
             {
                 var imagem = GeneralHelpers.TakeScreenshot(pasta);
             }
             else
             {
-                var imagem = Screenshot.TakeScreenshotProposta(numProposta);
-                //Functions.GerarDocumentoWord.CriaDocumentoEvidenciaProposta(imagem, numProposta);
+                var imagem = Screenshot.TakeScreenshot(x);
+                
             }
         }
-        public static string RetornaNumProposta(string value)
+        public static string RetornaValor(string value)
         {
             string regex = "[0-9]{9}";
             MatchCollection matches = new Regex(regex).Matches(value);
